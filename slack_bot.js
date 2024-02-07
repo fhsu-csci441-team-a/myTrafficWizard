414f597531a55b80be94793915fd8fd0abe1f026
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-02-07 01:34:28 -0800

    Changing Error Message Catcching
    Changed error message catching to troubleshoot.

--- slack_bot.js
+++ slack_bot.js
@@ -1,5 +1,10 @@
 fetch('https://my-traffic-wizard-node-js.onrender.com/postMessage')
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
   .then(data => console.log(data))
   .catch((error) => {
     console.error('Error:', error);
