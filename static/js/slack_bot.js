26c508966cb334fb348f3f7ef8a767f5ed0f4d81
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-02-27 01:37:05 -0800

    Modified URL for Fetch
    Modified URL for fetch from local to Render version.

--- static/js/slack_bot.js
+++ static/js/slack_bot.js
@@ -10,7 +10,8 @@ document.getElementById('userForm').addEventListener('submit', async function(ev
   };
 
   try {
-    const response = await fetch('/slack/postMessage', {
+    // use '/slack/postMessage' for local run or full URL for deployed app
+    const response = await fetch('https://mytrafficwizard.onrender.com/slack/postMessage', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
