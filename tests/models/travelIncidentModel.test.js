618d51567e28b073acec41d5a0b6383b56756833
fhsu-tbanderson <tbanderson@mail.fhsu.edu>
2024-03-30 15:48:06 -0500

    Initial commit of travel incident model test

---
+++ tests/models/travelIncidentModel.test.js
@@ -0,0 +1,45 @@
+const TravelIncidentModel = require('../../models/travelIncidentModel');
+require('dotenv').config();
+
+describe('TC-12 TravelIncidentModel', () => {
+    const validStartPoint = '37.3405,-97.6768';
+    const validEndPoint = '38.0339,-96.9834';
+    const validApiKey = process.env.TOMTOM_API_KEY;
+
+    const invalidStartPoint = 'invalid_start';
+    const invalidEndPoint = 'invalid_end';
+    const invalidApiKey = 'invalid_api_key';
+
+    it('Instantiates object with valid startPoint, endPoint, and API key, returns success message', async () => {
+        const travelIncidentModel = new TravelIncidentModel(validStartPoint, validEndPoint, validApiKey);
+        const result = await travelIncidentModel.getTravelIncidents();
+
+        expect(result).toHaveProperty('success', true);
+
+        if (result.data) {
+            expect(result.data[0]).toHaveProperty('iconCategory');
+            expect(result.data[0]).toHaveProperty('magnitudeOfDelay');
+            expect(result.data[0]).toHaveProperty('from');
+            expect(result.data[0]).toHaveProperty('to');
+
+        }
+
+
+    });
+
+    it('Instantiate object with invalid startPoint and enPoint, returns failure message', async () => {
+
+        const travelIncidentModel = new TravelIncidentModel(invalidStartPoint, invalidEndPoint, validApiKey);
+        const result = await travelIncidentModel.getTravelIncidents();
+        expect(result).toHaveProperty('success', false);
+
+    });
+
+    it('Instantiate object with invalid apiKey, returns failure message', async () => {
+
+        const travelIncidentModel = new TravelIncidentModel(validStartPoint, validEndPoint, invalidApiKey);
+        const result = await travelIncidentModel.getTravelIncidents();
+        expect(result).toHaveProperty('success', false);
+
+    });
+});
