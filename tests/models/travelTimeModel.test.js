c41ee2f8ce2e1c8a6526797ceb4f97990298b0d6
fhsu-tbanderson <tbanderson@mail.fhsu.edu>
2024-03-30 14:56:49 -0500

    Add test file for travel time model

---
+++ tests/models/travelTimeModel.test.js
@@ -0,0 +1,42 @@
+const TravelTimeModel = require('../../models/travelTimeModel');
+require('dotenv').config();
+
+describe('TC-11 TravelTimeModel', () => {
+    const validStartPoint = '52.41072,4.84239';
+    const validEndPoint = '52.377956,4.897070';
+    const validApiKey = process.env.TOMTOM_API_KEY;
+
+    const invalidStartPoint = 'invalid_start';
+    const invalidEndPoint = 'invalid_end';
+    const invalidApiKey = 'invalid_api_key';
+
+    it('Instantiates object with valid startPoint, endPoint, and API key, returns success message', async () => {
+        const travelTimeModel = new TravelTimeModel(validStartPoint, validEndPoint, validApiKey);
+        const result = await travelTimeModel.getTravelTimes();
+        console.log(result);
+
+
+        expect(result).toHaveProperty('success', true);
+        expect(result.data).toHaveProperty('historicalTravelTimeMinutes');
+        expect(result.data).toHaveProperty('liveTrafficTravelTimeMinutes');
+        expect(result.data).toHaveProperty('noTrafficTravelTimeMinutes');
+        expect(result.data).toHaveProperty('travelDelayMinutes');
+
+    });
+
+    it('Instantiate object with invalid startPoint and enPoint, returns failure message', async () => {
+
+        const travelTimeModel = new TravelTimeModel(invalidStartPoint, invalidEndPoint, validApiKey);
+        const result = await travelTimeModel.getTravelTimes();
+        expect(result).toHaveProperty('success', false);
+
+    });
+
+    it('Instantiate object with invalid apiKey, returns failure message', async () => {
+
+        const travelTimeModel = new TravelTimeModel(validStartPoint, validEndPoint, invalidApiKey);
+        const result = await travelTimeModel.getTravelTimes();
+        expect(result).toHaveProperty('success', false);
+
+    });
+});
