5013e7a8432231a2970aaff7c29338cfbd3ce449
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-04-17 03:16:24 -0700

    Updated DiscordBotModel Methods and Calls
    Updated the methods in DiscordBotModel so that the destroy() and login() methods could be called from DiscordBotController. This seems to change the timing enough to prevent hanging, while calling these within the class itself causes hanging.

--- models/discordBotModel.js
+++ models/discordBotModel.js
@@ -14,7 +14,7 @@ class DiscordBotModel {
 
   #client;
 
-  // creates new connection to Discord Bot each time instantiated
+  // creates new client connection to Discord Bot
   constructor() {
     // set intents (permissions) for Discord bot
     const myIntents = new IntentsBitField();
@@ -27,54 +27,54 @@ class DiscordBotModel {
       IntentsBitField.Flags.DirectMessages
     );
 
-    this.#initiateDiscordBot(myIntents);
-  }
-
-  // implements await functions used during DiscordBotModel creation
-  async #initiateDiscordBot(myIntents) {
     // create client to create connection to Discord
     this.#client = new Client({ intents: myIntents });
-    await this.#client.login(process.env.DISCORD_API_TOKEN);
 
-    // Discord Bot event handler function
-    const readyFunction = () => {
-      console.log(`Logged in as ${this.#client.user.tag}`);
-    };
-
-    // creating Discord Bot event listener
-    await this.#client.on('ready', readyFunction);
+    // Discord Bot event handler/listener function
+    this.readyPromise = new Promise(resolve => {
+      this.#client.on('ready', () => {
+        console.log(`Logged in as ${this.#client.user.tag}`);
+        resolve();
+      });
+    });
   }
 
-  // public method to close Discord client connection
-  async closeConnection() {
-    // wait for the client to become ready before destroying it
-    await new Promise(resolve => {
-      this.#client.once('ready', () => resolve());
-    });
+  // public method to login to Discord Bot client with API token
+  async login() {
+    try {
+      await this.#client.login(process.env.DISCORD_API_TOKEN);
+      await this.readyPromise;
+      return { code:200, message: `Discord client login successful`}
+    }
+    catch (error) {
+      console.error("Login failed:", error);
+      return { code: 500, message: `Discord Bot Model Login Failed:\n ${error}` };
+    }
+  }
 
-    // Destroy the client connection (after being ready)
-    await this.#client.destroy();
+  // public method to disconnect/destroy Discord client connection
+  async destroy() {
+    try {
+      // disconnect/destroy the client connection
+      await this.#client.destroy();
+      console.log("The Discord client has been disconnected")
+      return { code:200, message: `Discord client destroyed successfully`}
+    }
+    catch (error) {
+      console.error("Error while destroying Discord connection:", error);
+    }
   }
 
   // the postMessage method sends a message to Discord
   async postMessage(userID, formattedMessage) {
     try {
-      // Send the formatted message to Discord through the client
+      // send the formatted message to Discord through the client
       const response = await this.#client.users.send(userID, formattedMessage);
       console.log(response);
       return response;
-
-    } catch (error) {
-      return { code: 500, message: 'Discord Bot Model Failed' };
-
-    } finally {
-        try {
-          await this.closeConnection();
-          console.log("The Discord connection has been disconnected");
-          
-        } catch (error) {
-          console.error("Error while destroying Discord connection:", error);
-        }
+    } 
+    catch (error) {
+      return { code: 500, message: `Discord Bot Model Failed:\n ${error}` };
     }
   }
 }
