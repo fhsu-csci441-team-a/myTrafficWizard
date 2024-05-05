1e449a097127abc153b0cd83237db5e004f766e5
p-baldwin <p_baldwin@mail.fhsu.edu>
2024-05-04 23:13:25 -0500

    Removed unused gmailRoutes.js

--- routes/gmailRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes for Gmail functions through gmailController.js
-
-const express = require('express');
-const router = express.Router();
-const gmailController = require('../controllers/gmailController');
-
-// router.post('/send', gmailController.sendEmail);
-
-module.exports = router;
